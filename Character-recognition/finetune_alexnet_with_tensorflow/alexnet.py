7c3628bcae080e6daba74d556760d59312e30a5d
Abhay Doke <abhaydoke09@gmail.com>
2017-09-25 00:52:55 -0400

    changed alexnet

--- Character-recognition/finetune_alexnet_with_tensorflow/alexnet.py
+++ Character-recognition/finetune_alexnet_with_tensorflow/alexnet.py
@@ -6,19 +6,7 @@ Paper:
 Explanation can be found in my blog post:
 https://kratzert.github.io/2017/02/24/finetuning-alexnet-with-tensorflow.html
 
-This script enables finetuning AlexNet on any given Dataset with any number of
-classes. The structure of this script is strongly inspired by the fast.ai
-Deep Learning class by Jeremy Howard and Rachel Thomas, especially their vgg16
-finetuning script:
-Link:
-- https://github.com/fastai/courses/blob/master/deeplearning1/nbs/vgg16.py
 
-
-The pretrained weights can be downloaded here and should be placed in the same
-folder as this file:
-- http://www.cs.toronto.edu/~guerzhoy/tf_alexnet/
-
-@author: Frederik Kratzert (contact: f.kratzert(at)gmail.com)
 """
 
 import tensorflow as tf
